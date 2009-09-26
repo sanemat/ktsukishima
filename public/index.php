6bd7eedda4c1836868403153499b92dab51b2711
sanemat <o.gata.ken@gmail.com>
2009-09-26 20:55:05 +0900

    add sample and layout file
    add sample and layout file

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+# Loading limonade framework
+require_once dirname( __FILE__ ) . '/../lib/limonade.php';
+
+# Setting global options of our application
+function configure()
+{
+  option( 'root_dir', dirname( __FILE__ ) . '/..' );
+#  option( 'public_dir', option('root_dir').'/public/');
+  option( 'views_dir', option('root_dir').'/views/');
+#  option( 'controllers_dir', option('root_dir').'/controllers/');
+#  option( 'lib_dir', option('root_dir').'/lib/');
+
+}
+
+# Setting code that will be executed before each controller function
+function before()
+{
+  layout( 'layout.default.html.php' );
+}
+
+# Defining routes and controllers
+# matches GET /
+dispatch( '/', 'hello' );
+  function hello()
+  {
+    return 'Hello limonade!';
+  }
+
+# matches GET /page
+dispatch( '/:page', 'question_page' );
+  function question_page()
+  {
+    $page_name = params( 'page' );
+    $file_name = 'qa_' . $page_name . '.html.php';
+    if( preg_match( '/^[\w-\.]+$/D', $file_name ) !== 1 ){
+      halt( HTTP_FORBIDDEN, "An error occured while dispatch page " . h($file_name) );
+    }
+    elseif(!file_exists( file_path( option('views_dir'), $file_name ) ) ){
+      halt( HTTP_NOT_FOUND, "An error occured while dispatch page " . h($file_name) );
+    }
+
+    return render( $file_name );
+  }
+
+
+# Running the limonade app
+run();
---
+++ views/layout.default.html.php
@@ -0,0 +1,18 @@
+<?='<?xml version="1.0" encoding="UTF-8"?>';?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+
+<head>
+  <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
+  <meta name="keywords" content="(キーワード)" />
+  <meta name="description" content="(ページ説明)" />
+  <meta name="robots" content="index,follow" />
+  <title>(タイトル)</title>
+</head>
+
+<body>
+    (本文)
+</body>
+</html>
+
