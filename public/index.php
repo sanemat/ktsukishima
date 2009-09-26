28391a193ed64380c9ae8a24e3a510ac06229ca7
sanemat <o.gata.ken@gmail.com>
2009-09-27 05:18:15 +0900

    indent
    indent

--- public/index.php
+++ public/index.php
@@ -6,11 +6,9 @@ require_once dirname( __FILE__ ) . '/../lib/limonade.php';
 function configure()
 {
   option( 'root_dir', dirname( __FILE__ ) . '/..' );
-#  option( 'public_dir', option('root_dir').'/public/');
-  option( 'views_dir', option('root_dir').'/views/');
-#  option( 'controllers_dir', option('root_dir').'/controllers/');
-  option( 'lib_dir', option('root_dir').'/lib/');
-  set_include_path(option('lib_dir') . PATH_SEPARATOR . get_include_path());
+  option( 'views_dir', option( 'root_dir' ).'/views/' );
+  option( 'lib_dir', option( 'root_dir' ).'/lib/' );
+  set_include_path( option( 'lib_dir' ) . PATH_SEPARATOR . get_include_path() );
 
 }
 
@@ -20,25 +18,25 @@ function before()
   require_once 'Net/UserAgent/Mobile.php';
   option( 'agent', Net_UserAgent_Mobile::factory() );
   
-  $encoding = option('encoding');
+  $encoding = option( 'encoding' );
   $content_type = "application/xhtml+xml; charset={$encoding}";
-  if(option('agent')->isDoCoMo()){
+  if( option( 'agent' )->isDoCoMo() ){
     $encoding = 'Shift_JIS';
     $content_type = "application/xhtml+xml; charset={$encoding}";
   }
-  elseif(option('agent')->isSoftBank()){
+  elseif( option( 'agent' )->isSoftBank() ){
     $encoding = 'UTF-8';
     $content_type = "text/html; charset={$encoding}";
   }
-  elseif(option('agent')->isEZweb()){
+  elseif( option( 'agent' )->isEZweb() ){
     $encoding = 'Shift_JIS';
     $content_type = "text/html; charset={$encoding}";
   }
-  set('encoding', $encoding);
-  set('content_type', $content_type);
+  set( 'encoding', $encoding );
+  set( 'content_type', $content_type );
 
   require_once 'Text/Pictogram/Mobile.php';
-  $picObject = Text_Pictogram_Mobile::factory('docomo', 'utf-8');
+  $picObject = Text_Pictogram_Mobile::factory( 'docomo', 'utf-8' );
   set( 'emoji', $picObject->getFormattedPictogramsArray() );
 
   layout( 'layout.default.html.php' );
@@ -46,29 +44,29 @@ function before()
 }
 
 # Setting code that will be executed after each controller function
-function after($output)
+function after( $output )
 {
   require_once 'Text/Pictogram/Mobile.php';
   
-  if( option('agent')->isDoCoMo() ){
+  if( option( 'agent' )->isDoCoMo() ){
     $output = mb_convert_encoding( $output, 'SJIS-WIN', 'UTF-8');
-    $emoji = Text_Pictogram_Mobile::factory('docomo', 'sjis');
+    $emoji = Text_Pictogram_Mobile::factory( 'docomo', 'sjis' );
     $output = $emoji->replace( $output );
     if( !headers_sent() ) header( 'Content-Type: application/xhtml+xml; charset=Shift_JIS' );
   }
-  elseif( option('agent')->isSoftBank() ){
-    $emoji = Text_Pictogram_Mobile::factory('softbank', 'utf-8');
+  elseif( option( 'agent' )->isSoftBank() ){
+    $emoji = Text_Pictogram_Mobile::factory( 'softbank', 'utf-8' );
     $output = $emoji->replace( $output );
     if( !headers_sent() ) header( 'Content-Type: text/html; charset=UTF-8' );
   }
-  elseif( option('agent')->isEZweb() ){
-    $output = mb_convert_encoding( $output, 'SJIS-WIN', 'UTF-8');
-    $emoji = Text_Pictogram_Mobile::factory('au', 'sjis');
+  elseif( option( 'agent' )->isEZweb() ){
+    $output = mb_convert_encoding( $output, 'SJIS-WIN', 'UTF-8' );
+    $emoji = Text_Pictogram_Mobile::factory( 'au', 'sjis' );
     $output = $emoji->replace( $output );
     if( !headers_sent() ) header( 'Content-Type: text/html; charset=Shift_JIS' );
   }
   else{
-    $emoji = Text_Pictogram_Mobile::factory( null, 'utf-8');
+    $emoji = Text_Pictogram_Mobile::factory( null, 'utf-8' );
     $output = $emoji->replace( $output );
   }
 
@@ -90,10 +88,10 @@ dispatch( '/:page', 'question_page' );
     $page_name = params( 'page' );
     $file_name = 'qa_' . $page_name . '.html.php';
     if( preg_match( '/^[\w-\.]+$/D', $file_name ) !== 1 ){
-      halt( HTTP_FORBIDDEN, "An error occured while dispatch page " . h($file_name) );
+      halt( HTTP_FORBIDDEN, "An error occured while dispatch page " . h( $file_name ) );
     }
     elseif(!file_exists( file_path( option('views_dir'), $file_name ) ) ){
-      halt( HTTP_NOT_FOUND, "An error occured while dispatch page " . h($file_name) );
+      halt( HTTP_NOT_FOUND, "An error occured while dispatch page " . h( $file_name ) );
     }
 
     return render( $file_name );
