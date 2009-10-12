0c2d5269a0a73cec9dff114876383dc5d4cf9384
sanemat <o.gata.ken@gmail.com>
2009-10-12 20:37:02 +0900

    markup toppage
    markup toppage

--- views/top_page.html.php
+++ views/top_page.html.php
@@ -1,17 +1,17 @@
-<pre>
-ゴール
-
-index.php
+<?php set('title', 'how to start limonade');?>
+<h1>how to start limonade</h1>
+<p>
+ゴール<br />
+<br />
 サブディレクトリ以下にlimonadeで3キャリア対応のページを作る
-
- how_to_download
-
-1 limonade
-2 PEAR
-3 Net_UserAgent_Mobile
-4 HTML_CSS
-5 HTML_CSS_Mobile
-6 HTML_CSS_Selector2XPath
-7 HTML_Common
-8 Text_Pictgram_Mobile
-</pre>
+</p>
+<ul>
+<li><a href="<?=url_for('download_limonade');?>">limonadeダウンロードからhello world</a></li>
+<li>PEAR</li>
+<li>Net_UserAgent_Mobile</li>
+<li>HTML_CSS</li>
+<li>HTML_CSS_Mobile</li>
+<li>HTML_CSS_Selector2XPath</li>
+<li>HTML_Common</li>
+<li>Text_Pictgram_Mobile</li>
+</ul>
