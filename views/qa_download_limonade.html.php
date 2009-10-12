251c79dda80046dcad7adb8c9beb0ecca289545c
sanemat <o.gata.ken@gmail.com>
2009-10-12 20:36:24 +0900

    add file put
    add file put

--- views/qa_download_limonade.html.php
+++ views/qa_download_limonade.html.php
@@ -4,6 +4,7 @@
 <ul>
 <li>limonadeをダウンロードする</li>
 <li>動作確認</li>
+<li>ファイル構成</li>
 <li>ドキュメントルートの上に移動</li>
 </ul>
 
@@ -28,6 +29,70 @@ $ git checkout origin/0.4
 </pre>
 </code>
 </p>
+<h2>ファイル構成</h2>
+<code>
+<pre>
+$ tree .
+.
+|-- AUTHORS
+|-- CHANGES
+|-- LICENSE
+|-- LISEZMOI.mkd
+|-- README.mkd
+|-- TODO
+|-- examples
+|   |-- example01
+|   |   |-- index.php
+|   |   `-- public
+|   |       |-- soda_glass.jpg
+|   |       `-- soda_glass.thb.jpg
+|   |-- example02
+|   |   `-- index.php
+|   |-- index.php
+|   `-- urlrewrite
+|       |-- htaccess.conf
+|       `-- lighttpd.conf
+|-- lib
+|   |-- limonade
+|   |   |-- abstract.php
+|   |   |-- assertions.php
+|   |   |-- public
+|   |   |   |-- css
+|   |   |   |   `-- screen.css
+|   |   |   `-- img
+|   |   |       `-- bg_header.png
+|   |   |-- tests.php
+|   |   `-- views
+|   |       |-- _debug.html.php
+|   |       |-- _notices.html.php
+|   |       |-- default_layout.php
+|   |       `-- error.html.php
+|   `-- limonade.php
+`-- tests
+    |-- all.php
+    |-- apps
+    |   |-- 00-empty.php
+    |   `-- 01-hello_world.php
+    |-- data
+    |   |-- deer.jpg
+    |   |-- empty_text_file.txt
+    |   `-- lib0
+    |       |-- a.php
+    |       |-- b.php
+    |       `-- c.php
+    |-- file.php
+    |-- functional.php
+    |-- helpers
+    |   `-- show_request_uri.php
+    |-- main.php
+    |-- output.php
+    |-- request.php
+    |-- router.php
+    `-- tests.php
+
+16 directories, 39 files
+</pre>
+</code>
 <h2>動作確認</h2>
 <p>
 webサーバからアクセスできるようにする
